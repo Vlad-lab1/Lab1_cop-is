c5d1c2088026dacd8a415a76219503f2e8889b6e
Vlad-lab1 <vlab.ruslo@gmail.com>
2026-03-06 16:36:06 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+echo "# Lab1_cop" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin git@github.com:Vlad-lab1/Lab1_cop.git
+git push -u origin main
+
+
